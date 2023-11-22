fb9d14d0db89fdaf0d2a8acc1b35e71af36cac98
Yan Oliveira <yan.o.s.martins@gmail.com>
2023-11-22 17:33:21 -0300

    atribuindo proprieades aos campos

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,10 @@
 import './CampoTexto.css'
 
-const CampoTexto = () => {  //isso é uma arrow function 
+const CampoTexto = (props) => {  //isso é uma arrow function ||props é o nome comum pra essa variavel de propriedades.
     return(
         <div className="campo-texto">
-            <label>Nome</label>
-            <input  placeholder='Digite o seu nome'/>
+            <label>{props.label}</label>
+            <input  placeholder={props.placeholder}/>
         </div>
     )
 }
