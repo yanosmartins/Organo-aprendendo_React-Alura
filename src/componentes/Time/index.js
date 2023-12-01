b50783c51d4eda34a90be58d7402ecd6715947a7
Yan Oliveira <yan.o.s.martins@gmail.com>
2023-12-01 14:01:40 -0300

    comentário importante

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,10 +1,11 @@
 import './Time.css'
 
 const Time = (props) =>{
-    const css = { backgroundColor: props.corSecundaria }
+    const css = { backgroundColor: props.corSecundaria } //alternativa de atribuição de estilo
+    //dentro deste estilo temos duas chaves pois dentro de uma, o react espera receber uma variavel... dentro de duas ele passa a receber um objeto
     return (
-        <section className='time' style={css}>
-            <h3  style={{borderColor: props.corPrimaria}} >{props.nome}</h3>
+        <section className='time' style={css}> 
+            <h3  style={{borderColor: props.corPrimaria}} >{props.nome}</h3> 
         </section>
     )
     
