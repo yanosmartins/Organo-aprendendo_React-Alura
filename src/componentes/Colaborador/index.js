abaa556e2213a4769f8e42ca383187f5271cd11b
Yan Oliveira <yan.o.s.martins@gmail.com>
2023-12-03 11:02:20 -0300

    colaborador estatico e truque foto github

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () =>{
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/yanosmartins.png' alt ='Yan Oliveira'/>
+        </div>
+        <div className='rodape'>
+            <h4>Yan Oliveira</h4>
+            <h5>Desenvolvedor FullStack</h5>
+        </div>
+
+    </div>)
+}
+
+export default Colaborador
